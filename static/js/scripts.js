0d11c9d39f6df7c480040402ade7cd663270531b
MIrko Sertic <mirko.sertic@web.de>
2017-02-02 20:07:55 +0100

    Test with pygments

---
+++ static/js/scripts.js
@@ -0,0 +1,12 @@
+jQuery(function($) {
+
+    /* ============================================================ */
+    /* Scroll To Top */
+    /* ============================================================ */
+
+    $('.js-jump-top').on('click', function(e) {
+        e.preventDefault();
+
+        $('html, body').animate({'scrollTop': 0});
+    });
+});
