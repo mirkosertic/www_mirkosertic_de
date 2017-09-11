46ecf68fe98a79684548955446713271b3594e17
Mirko Sertic <mirko.sertic@web.de>
2017-09-11 17:38:50 +0200

    Debugging build script

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,8 +3,6 @@ import htmlmin from 'gulp-htmlmin'
 import runSequence from 'run-sequence'
 import shell from 'gulp-shell'
 
-gulp.task('hugo-build', shell.task(['hugo']))
-
 gulp.task('minify-html', () => {
     return gulp.src('public/**/*.html')
         .pipe(htmlmin({
@@ -17,6 +15,6 @@ gulp.task('minify-html', () => {
         .pipe(gulp.dest('./public'))
 })
 
-gulp.task('build', ['hugo-build'], (callback) => {
+gulp.task('build', [], (callback) => {
     runSequence('minify-html', callback)
 })
