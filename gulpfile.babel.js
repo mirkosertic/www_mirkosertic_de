c442365f611c3d44b68d8b56f5d5798cc78977cf
Mirko Sertic <mirko.sertic@web.de>
2017-09-11 17:44:16 +0200

    Revert "Debugging build script"
    This reverts commit 3a87cf8

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,8 @@ import htmlmin from 'gulp-htmlmin'
 import runSequence from 'run-sequence'
 import shell from 'gulp-shell'
 
+gulp.task('hugo-build', shell.task(['hugo']))
+
 gulp.task('minify-html', () => {
     return gulp.src('public/**/*.html')
         .pipe(htmlmin({
@@ -15,6 +17,6 @@ gulp.task('minify-html', () => {
         .pipe(gulp.dest('./public'))
 })
 
-gulp.task('build', [], (callback) => {
+gulp.task('build', ['hugo-build'], (callback) => {
     runSequence('minify-html', callback)
 })
