4d4f09b363bfe90b5f37222005bf93dd64dcf7c1
Mirko Sertic <mirko.sertic@web.de>
2018-04-18 18:24:02 +0200

    Working on new theme

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,11 @@ import htmlmin from 'gulp-htmlmin'
 import runSequence from 'run-sequence'
 import shell from 'gulp-shell'
 
+var ftp = require('vinyl-ftp');
+var gutil = require('gulp-util');
+var minimist = require('minimist');
+var args = minimist(process.argv.slice(2));
+
 gulp.task('hugo-build', shell.task(['hugo']))
 
 gulp.task('minify-html', () => {
@@ -15,8 +20,21 @@ gulp.task('minify-html', () => {
             useShortDoctype: false,
         }))
         .pipe(gulp.dest('./public'))
-})
+});
 
 gulp.task('build', ['hugo-build'], (callback) => {
     runSequence('minify-html', callback)
-})
+});
+
+gulp.task('deploy', function() {
+    var remotePath = '/www/';
+    var conn = ftp.create({
+        host: 'w0077e1b.kasserver.com',
+        user: args.user,
+        password: args.password,
+        log: gutil.log
+    });
+    gulp.src(['./public/**/*.*'])
+        .pipe(conn.newer(remotePath))
+        .pipe(conn.dest(remotePath));
+});
--- upload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-lftp -f "
-open w0077e1b.kasserver.com
-user $FTPUSR $FTPPWD
-mirror -v -R ./public /www
-bye"
