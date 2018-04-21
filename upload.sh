fcc92ea88bee5dce111ddfc587919e5fba17208b
Mirko Sertic <mirko.sertic@web.de>
2018-04-22 00:05:17 +0200

    Working on new theme

--- upload.sh
+++ upload.sh
@@ -3,7 +3,5 @@
 pwd
 ls -la
 
-if  [[ $TRAVIS_PULL_REQUEST = "false" ]]
-    cd public || exit
-    ncftpput -R -v -u "$FTPUSR" -p "$FTPPWD" "w0077e1b.kasserver.com" /www/testsite .
-fi
+cd public || exit
+ncftpput -R -v -u "$FTPUSR" -p "$FTPPWD" "w0077e1b.kasserver.com" /www/testsite .
