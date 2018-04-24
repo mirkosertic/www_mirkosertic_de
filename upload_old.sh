6e2c4799f9e50fa3faa349246a75338230c075fe
Mirko Sertic <mirko.sertic@web.de>
2018-04-24 21:55:59 +0200

    Working on new theme

--- upload_old.sh
+++ upload_old.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-lftp -f "
-open w0077e1b.kasserver.com
-user $FTPUSR $FTPPWD
-set ftp:passive-mode off
-cd www
-mirror -R ./public
-bye"
+cd public
+find . -type f -exec curl -u $FTPUSR:$FTPPWD --ftp-create-dirs -T {} ftp://w0077e1b.kasserver.com/www/testsite2/{} \;
