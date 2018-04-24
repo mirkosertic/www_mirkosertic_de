6a1db326f14faf3990a00322e7adbdedb7748a40
Mirko Sertic <mirko.sertic@web.de>
2018-04-24 22:11:32 +0200

    Working on new theme

--- upload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pwd
-ls -la
-
-cd public || exit
-ncftpput -R -v -u "$FTPUSR" -p "$FTPPWD" "w0077e1b.kasserver.com" /www/testsite .
--- upload_old.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd public
-find . -type f -exec curl -u $FTPUSR:$FTPPWD --ftp-create-dirs -T {} ftp://w0077e1b.kasserver.com/www/testsite2/{} \;
