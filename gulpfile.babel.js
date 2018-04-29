c160a4ee9236bc0a1ed4ac62b51ce3e77e1e6985
Mirko Sertic <mirko.sertic@web.de>
2018-04-29 11:47:49 +0200

    Working on new theme

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,12 +3,18 @@ import htmlmin from 'gulp-htmlmin'
 import runSequence from 'run-sequence'
 import shell from 'gulp-shell'
 
-var ftp = require('vinyl-ftp');
-var gutil = require('gulp-util');
-var minimist = require('minimist');
-var args = minimist(process.argv.slice(2));
+var imagemin = require('gulp-imagemin');
 
-gulp.task('hugo-build', shell.task(['hugo']))
+gulp.task('hugo-build', shell.task(['hugo']));
+
+gulp.task('imagemin', () => {
+    return gulp.src('static/media/*')
+        .pipe(imagemin([
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.jpegtran({progressive: true}),
+        ]), {verbose: true})
+        .pipe(gulp.dest('public/media'))
+});
 
 gulp.task('minify-html', () => {
     return gulp.src('public/**/*.html')
@@ -22,6 +28,6 @@ gulp.task('minify-html', () => {
         .pipe(gulp.dest('./public'))
 });
 
-gulp.task('build', ['hugo-build'], (callback) => {
-    runSequence('minify-html', callback)
+gulp.task('build', ['hugo-build',], (callback) => {
+    runSequence('minify-html', 'imagemin', callback)
 });
