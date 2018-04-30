e680179c489a0513fc4a9a1536daebf12566f295
Mirko Sertic <mirko.sertic@web.de>
2018-04-30 12:17:56 +0200

    Minor updates

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -13,8 +13,29 @@ gulp.task('imagemin', () => {
         .pipe(imagemin([
             imagemin.optipng({optimizationLevel: 5}),
             imagemin.jpegtran({progressive: true}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
         ]), {verbose: true})
         .pipe(gulp.dest('public/media'))
+});
+
+gulp.task('imagemin-assets', () => {
+    return gulp.src('themes/boot/static/assets/*')
+        .pipe(imagemin([
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.jpegtran({progressive: true}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ]), {verbose: true})
+        .pipe(gulp.dest('themes/boot/static/assets'))
 });
 
 gulp.task('minify-html', () => {
@@ -30,13 +51,13 @@ gulp.task('minify-html', () => {
 });
 
 gulp.task('cssmin', function () {
-    return gulp.src(['public/css/site.css', 'public/css/bootstrap.css'])
+    return gulp.src(['public/css/site.css'])
         .pipe(uncss({
             html: ['public/*.html', 'public/post/**/*.html', 'public/tags/**/*.html', 'public/global/**/*.html', 'public/blog/**/*.html']
         }))
-        .pipe(gulp.dest('./public/css'));
+        .pipe(gulp.dest('public/css'));
 });
 
 gulp.task('build', ['hugo-build',], (callback) => {
-    runSequence('minify-html', 'imagemin', callback)
+    runSequence('minify-html', 'imagemin', 'cssmin', callback)
 });
