e31240c9cae7141d9799c278a149e9da08ed023b
mirkosertic <mirko.sertic@web.de>
2021-02-02 10:15:09 +0100

    Build with GitHub Actions

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -57,4 +57,4 @@ gulp.task('cssmin', function () {
         .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('build', gulp.series(['hugo-build', 'minify-html', 'imagemin', 'cssmin']));
+gulp.task('build', gulp.series(['hugo-build', 'minify-html', 'imagemin']));
