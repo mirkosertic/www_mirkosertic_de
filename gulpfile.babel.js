91a775d2d1bcfb7fb1550d77da6d0d41122eccf8
mirkosertic <mirko.sertic@web.de>
2026-04-21 20:46:38 +0200

    Updated `package-lock.json` with new versions of `openai`, Babel dependencies, and added `sharp`, `svgo` along with other dependency cleanups.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,49 +1,73 @@
 import gulp from 'gulp'
-import htmlmin from 'gulp-htmlmin'
+import { minify as minifyHtml } from '@minify-html/node'
 import shell from 'gulp-shell'
+import { Transform } from 'stream'
+import sharp from 'sharp'
+import { optimize as svgoOptimize } from 'svgo'
+import { extname } from 'path'
 
-var imagemin = require('gulp-imagemin');
+function optimizeImages() {
+    return new Transform({
+        objectMode: true,
+        transform(file, enc, callback) {
+            if (file.isNull()) {
+                return callback(null, file);
+            }
+
+            const ext = extname(file.path).toLowerCase();
+
+            if (ext === '.png') {
+                sharp(file.contents)
+                    .png({ compressionLevel: 9 })
+                    .toBuffer()
+                    .then(buf => { file.contents = buf; callback(null, file); })
+                    .catch(() => callback(null, file));
+            } else if (ext === '.jpg' || ext === '.jpeg') {
+                sharp(file.contents)
+                    .jpeg({ progressive: true })
+                    .toBuffer()
+                    .then(buf => { file.contents = buf; callback(null, file); })
+                    .catch(() => callback(null, file));
+            } else if (ext === '.svg') {
+                const result = svgoOptimize(file.contents.toString(), {
+                    plugins: [{
+                        name: 'preset-default',
+                        params: { overrides: { cleanupIds: false } }
+                    }]
+                });
+                file.contents = Buffer.from(result.data);
+                callback(null, file);
+            } else {
+                callback(null, file);
+            }
+        }
+    });
+}
 
 gulp.task('hugo-build', shell.task(['hugo']));
 
 gulp.task('imagemin', () => {
-    return gulp.src('static/media/*')
-        .pipe(imagemin([
-            imagemin.optipng({optimizationLevel: 5}),
-            imagemin.jpegtran({progressive: true}),
-            imagemin.svgo({
-                plugins: [
-                    {removeViewBox: true},
-                    {cleanupIDs: false}
-                ]
-            })
-        ]), {verbose: true})
+    return gulp.src('static/media/**/*', { nodir: true })
+        .pipe(optimizeImages())
         .pipe(gulp.dest('public/media'))
 });
 
 gulp.task('imagemin-assets', () => {
-    return gulp.src('themes/boot/static/assets/*')
-        .pipe(imagemin([
-            imagemin.optipng({optimizationLevel: 5}),
-            imagemin.jpegtran({progressive: true}),
-            imagemin.svgo({
-                plugins: [
-                    {removeViewBox: true},
-                    {cleanupIDs: false}
-                ]
-            })
-        ]), {verbose: true})
+    return gulp.src('themes/boot/static/assets/**/*', { nodir: true })
+        .pipe(optimizeImages())
         .pipe(gulp.dest('themes/boot/static/assets'))
 });
 
 gulp.task('minify-html', () => {
+    const cfg = { keep_comments: true, minify_css: false, minify_js: false };
     return gulp.src('public/**/*.html')
-        .pipe(htmlmin({
-            collapseWhitespace: true,
-            minifyCSS: false,
-            minifyJS: false,
-            removeComments: false,
-            useShortDoctype: false,
+        .pipe(new Transform({
+            objectMode: true,
+            transform(file, enc, callback) {
+                if (file.isNull()) return callback(null, file);
+                file.contents = minifyHtml(file.contents, cfg);
+                callback(null, file);
+            }
         }))
         .pipe(gulp.dest('./public'))
 });
